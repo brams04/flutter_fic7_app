cba898059f7ee2b3eb898f0d0368a434b20f83cb
brams04 <putratoang@gmail.com>
2023-12-24 12:18:02 +0700

    meet 11

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://79b6-2404-8000-1028-10fb-7481-22ff-5395-c852.ngrok-free.app';
+  static const baseUrl = 'https://5df9-2404-8000-1028-10fb-7481-22ff-5395-c852.ngrok-free.app';
 }
